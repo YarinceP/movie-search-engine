bf1c39e9b34754b989b4fcc850871bad39dca908
YarinceP <padilla.yarince@gmail.com>
2020-05-25 02:48:53 -0600

    create SearchBox component

--- src/components/SearchBox.vue
+++ src/components/SearchBox.vue
@@ -1,8 +1,19 @@
 <template>
-  <form action class="search-box">
-    <input type="text" />
-    <button >Buscar</button>
-  </form>
+  <v-form>
+    <v-container>
+      <v-row>
+        <h1 >Search your favorite movie</h1>
+      </v-row>
+      <v-row>
+        <v-col cols="10">
+          <v-text-field label="Cinema" placeholder="Example" outlined />
+        </v-col>
+        <v-col cols="2">
+          <v-btn color="warning" large rounded>Search</v-btn>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-form>
 </template>
 
 <script>
