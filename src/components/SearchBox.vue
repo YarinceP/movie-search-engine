a67304449f1be527b60708e9b3fdc14c7618190b
YarinceP <padilla.yarince@gmail.com>
2020-05-24 21:43:11 -0600

    create search component

---
+++ src/components/SearchBox.vue
@@ -0,0 +1,13 @@
+<template>
+  <form action class="search-box">
+    <input type="text" />
+    <button >Buscar</button>
+  </form>
+</template>
+
+<script>
+export default {
+    name: 'SearchBox'
+}
+</script>
+
