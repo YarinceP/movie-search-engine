a306b1055be1ebdd732d4cf4da644b73ed4cac4a
YarinceP <padilla.yarince@gmail.com>
2020-05-26 06:04:50 -0600

    format code

--- src/components/MovieList.vue
+++ src/components/MovieList.vue
@@ -1,45 +1,33 @@
 <template>
   <v-container>
-      <v-row>
-          <v-col
-          v-for="movie in movies.results"
-          :key="movie.id"
-          cols="12"
-          md="6"
-          lg="4"
-          sm="4"
-          >
-          <MovieCard
-          :title="movie.title"
-          :posterPath="movie.poster_path" 
-          >
-          </MovieCard>
-          </v-col>
-      </v-row>
+    <v-row>
+      <v-col v-for="movie in movies.results" :key="movie.id" cols="12" md="6" lg="4" sm="4">
+        <MovieCard :title="movie.title" :posterPath="movie.poster_path"></MovieCard>
+      </v-col>
+    </v-row>
   </v-container>
 </template>
 
 <script>
-import MovieCard from '@/components/MovieCard.vue'
-import {mapActions, mapState} from 'vuex'
-import api from '@/api'
+import MovieCard from "@/components/MovieCard.vue";
+import { mapActions, mapState } from "vuex";
+import api from "@/api";
 export default {
-    name: 'MovieList',
-    components:{
-        MovieCard
-    },
-    methods: {
-        ...mapActions(['getMoviesList'])  
-    },
-    computed:{
-        ...mapState(['movies'])
-    },
-    mounted(){
-       this.getMoviesList()
-    },
-}
+  name: "MovieList",
+  components: {
+    MovieCard
+  },
+  methods: {
+    ...mapActions(["getMoviesList"])
+  },
+  computed: {
+    ...mapState(["movies"])
+  },
+  mounted() {
+    this.getMoviesList();
+  }
+};
 </script>
 
 <style>
-
 </style>
