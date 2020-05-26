fa5cae344ad07213534310efe7eb388362095c3d
YarinceP <padilla.yarince@gmail.com>
2020-05-26 06:02:26 -0600

    create a component TitlePage

---
+++ src/components/TitlePage.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-container>
+      <v-row>
+          <v-col cols="12">
+              <h1>{{ title }}</h1>
+          </v-col>
+      </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+    name: 'TitlePage',
+    props:['title']
+}
+</script>
+
+<style>
+
+</style>
