def673d5f2eb4ed271417d51375b3dc3aaede673
YarinceP <padilla.yarince@gmail.com>
2020-05-26 00:45:52 -0600

    create component MovieCard

---
+++ src/components/MovieCard.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-hover>
+    <v-card >
+        <v-img :src="posterComp" :title="title" width="250" height="400">
+
+        </v-img>
+    </v-card>
+  </v-hover>
+</template>
+
+<script>
+import { mapState, mapMutations, mapActions } from "vuex";
+export default {
+  name: "movie-card",
+
+};
+</script>
