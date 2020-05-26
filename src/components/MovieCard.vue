841c2a1c43de6c81dcb1dfc1121c6992bc6072c2
YarinceP <padilla.yarince@gmail.com>
2020-05-26 05:25:52 -0600

    add props,mixins. import posterposter. style card,hover

--- src/components/MovieCard.vue
+++ src/components/MovieCard.vue
@@ -1,17 +1,19 @@
 <template>
-  <v-hover>
-    <v-card >
-        <v-img :src="posterComp" :title="title" width="250" height="400">
-
-        </v-img>
+  <v-hover v-slot:default="{ hover }">
+    <v-card :elevation="hover ? 21 : 2" width="300" shaped>
+      <v-img :src="posterComp" width="300" height="450">
+      </v-img>
+      <v-card-text class="teal white--text center--text">{{title}}</v-card-text>
     </v-card>
   </v-hover>
 </template>
 
 <script>
 import { mapState, mapMutations, mapActions } from "vuex";
+import posterMixin from "@/api/poster.js";
 export default {
   name: "movie-card",
-
+  mixins: [posterMixin],
+  props: ["title", "posterPath"]
 };
 </script>
