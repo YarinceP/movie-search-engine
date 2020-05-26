ea720ec049a5c6077bfd3f0c6b234a8a89fe5f9f
YarinceP <padilla.yarince@gmail.com>
2020-05-26 00:46:09 -0600

    create component MovieList

---
+++ src/components/MovieList.vue
@@ -0,0 +1,38 @@
+<template>
+  <v-container>
+      <v-row>
+          <v-col
+          v-for="n in 3"
+          :key="n"
+          cols="12"
+          sm="4"
+          >
+
+          </v-col>
+      </v-row>
+  </v-container>
+</template>
+
+<script>
+import MovieCard from '@/components/MovieCard.vue'
+import api from '@/api'
+export default {
+    name: 'MovieList',
+    data() {
+        return {
+            movies:null
+        }
+    },
+    methods: {
+        api:getMovies('a',1)
+        .then(console.log(response)
+        )
+        
+        
+    },
+}
+</script>
+
+<style>
+
+</style>
