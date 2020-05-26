741af24920e657ecfc15fcdc579f3b185917ebd4
YarinceP <padilla.yarince@gmail.com>
2020-05-26 05:50:32 -0600

    add component  MovieList

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,19 @@
 <template>
   <v-container>
     <SearchBox></SearchBox>
+    <MovieList></MovieList>
   </v-container>
 </template>
 
 <script>
 // @ is an alias to /src
 import SearchBox from "@/components/SearchBox.vue";
+import MovieList from "@/components/MovieList.vue";
 export default {
   name: "Home",
   components: {
     SearchBox,
+    MovieList
   }
 };
 </script>
