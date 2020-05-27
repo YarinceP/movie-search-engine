c7e74a7be5bbc0ee3914721ceb0b90e1055aaa08
YarinceP <padilla.yarince@gmail.com>
2020-05-27 15:11:09 -0600

    changes on store

--- src/store/index.js
+++ src/store/index.js
@@ -8,23 +8,31 @@ export default new Vuex.Store({
   state: {
     searchbox:{
       query: '',
+      page:   1,
+      total_pages: null,
     },
     movies:[]
-     
+
   },
   mutations: {
     showQuery(state,query){
       state.searchbox.query = query;
+      state.searchbox.page = 1;
       console.log(state.searchbox.query);
     },
     loadMovies(state,movies){
       state.movies = movies
+      state.searchbox.total_pages = movies.total_pages
+    },
+    resetData(state){
+      state.movies = []
     }
   },
   actions: {
     async getMoviesList(context,query){
-      let movies = await api.getMovies(query,1)
-         console.log(movies)
+      let movies = await api.getMovies(query,context.state.searchbox.page)
+         console.log(movies);
+         console.log(context.state.searchbox.page);
       context.commit('loadMovies',movies)
     },
   },
