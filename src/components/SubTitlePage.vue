f30e7273515dea7ee02ce41baf77298aa7bcc880
YarinceP <padilla.yarince@gmail.com>
2020-05-27 16:00:58 -0600

    create component SubTitlePage

---
+++ src/components/SubTitlePage.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-row>
+    <v-col>
+      <v-banner single-line>
+        <v-icon slot="icon" color="error" size="36">mdi-movie</v-icon>
+        {{title}}
+      </v-banner>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+  name: "SubTitlePage",
+  props: ["title"]
+};
+</script>
