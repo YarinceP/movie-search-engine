83bd485e1dd31fe99469f601216d1cd002beff73
YarinceP <padilla.yarince@gmail.com>
2020-05-27 15:19:37 -0600

    create component Pagination

---
+++ src/components/Pagination.vue
@@ -0,0 +1,35 @@
+<template>
+    <v-container>
+        <v-row justify="center">
+            <v-col>
+                <v-pagination
+                        v-model="searchbox.page"
+                        class="my-4"
+                        :length="searchbox.total_pages"
+                        :total-visible="15"
+                        @input="goToPage()"
+                ></v-pagination>
+            </v-col>
+        </v-row>
+    </v-container>
+</template>
+
+<script>
+    import {mapActions, mapMutations, mapState} from "vuex";
+
+    export default {
+        name: "Pagination",
+        computed: {
+            ...mapState(['searchbox']),
+        },
+        methods: {
+            ...mapMutations(['resetData']),
+            ...mapActions(['getMoviesList']),
+            goToPage() {//TODO: Hacer que el query solo se actualice al buscar.
+                this.resetData()
+                this.getMoviesList(this.searchbox.query)
+            }
+
+        },
+    };
+</script>
