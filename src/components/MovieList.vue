a9d353898dc2febc9085cdef1cfbe8f9acde1e08
YarinceP <padilla.yarince@gmail.com>
2020-05-27 16:02:12 -0600

    add SubTitlePage

--- src/components/MovieList.vue
+++ src/components/MovieList.vue
@@ -1,5 +1,8 @@
 <template>
   <v-container>
+    <SubTitlePage
+            v-if="movies.length !== 0"
+            :title="movies.length === 0 ? '' : 'Se encontraron ' + formatNumber + ' resultados'"></SubTitlePage>
     <v-row>
       <v-col v-for="movie in movies.results" :key="movie.id" cols="12" md="6" lg="4" sm="4">
         <MovieCard :title="movie.title" :posterPath="movie.poster_path"></MovieCard>
@@ -10,15 +13,20 @@
 
 <script>
 import MovieCard from "@/components/MovieCard.vue";
+import SubTitlePage from "@/components/SubTitlePage.vue";
 import { mapActions, mapState } from "vuex";
 import api from "@/api";
 export default {
   name: "MovieList",
   components: {
-    MovieCard
+    MovieCard,
+    SubTitlePage,
   },
   computed: {
-    ...mapState(["movies"])
+    ...mapState(["movies"]),
+    formatNumber(){
+     return new Intl.NumberFormat().format(this.movies.total_results)
+    }
   },
 };
 </script>
