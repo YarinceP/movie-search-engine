e824481d6eceef5c5987299e321939c0589f2e3c
YarinceP <padilla.yarince@gmail.com>
2020-05-27 21:30:17 -0600

    add new route to movie view

--- src/router/index.js
+++ src/router/index.js
@@ -17,7 +17,13 @@ Vue.use(VueRouter)
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+  },
+      {
+          path: '/movie/:id',
+          name: 'Movie',
+          component: () => import(/* webpackChunkName: "movie" */ '../views/Movie.vue')
+      }
+
 ]
 
 const router = new VueRouter({
